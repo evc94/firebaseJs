7ac5e01aee3e16548e238bb305f8423b5ae3ae29
Estuardo V <estuardov@onecodedev.com>
2017-10-05 20:38:44 -0600

    Get Data

--- js/sitelist.js
+++ js/sitelist.js
@@ -1,7 +1,9 @@
 var form = $('#form-site');
+var formData = $('#tbody-sites');
 var refData;
 $(document).ready(function(){
     refData = firebase.database().ref().child('sites');
+    getData();
 });
 
 
@@ -14,3 +16,23 @@ form.submit(function(event){
         description: event.target.description.value
     });
 });
+
+function getData(){
+    refData.on('value',function(snap){
+        var allData = snap.val();
+        var rows = "";
+        for (var index in allData) {
+            rows += "<tr>"+
+                    "<td> "+ allData[index].top+"</td>"+
+                    "<td> "+ allData[index].name+"</td>"+
+                    "<td> "+ allData[index].url+"</td>"+
+                    "<td> "+ allData[index].description+"</td>"+
+                    "<td> <div class=\"btn-group\">"+
+                    "<a href=\"\" class=\"btn btn-sm btn-warning\">Editar</a>"+
+                    "<a href=\"\" class=\"btn btn-sm btn-danger\">Eliminar</a>"+
+                    "</div> </td>"+
+                    "</tr>";
+        }
+        formData.html(rows);
+    })
+}
