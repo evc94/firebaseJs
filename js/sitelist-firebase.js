27bdc9c0339b418c37e855cbe8461473087afc9f
Estuardo V <estuardov@onecodedev.com>
2017-10-05 19:48:50 -0600

    Implements firebase

---
+++ js/sitelist-firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAxSuocY-hZuAvifRyTrUzM4F56TZcOHaU",
+  authDomain: "sitelist-141e5.firebaseapp.com",
+  databaseURL: "https://sitelist-141e5.firebaseio.com",
+  projectId: "sitelist-141e5",
+  storageBucket: "sitelist-141e5.appspot.com",
+  messagingSenderId: "194581619091"
+};
+firebase.initializeApp(config);
